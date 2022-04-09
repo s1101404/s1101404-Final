70f94af6a3abf5dff7caaa1cdee3ad50e17fa4fa
s1101404 <s1101404@mail.yzu.edu.tw>
2022-04-09 23:04:57 +0800

    2

--- assets/js/work2/work2.js
+++ assets/js/work2/work2.js
@@ -1,14 +1,21 @@
-function login() {
-    $("#courseTable").empty();
-    $("#courseTable").append("<tr><th>場次</th><th>時間</th><th>主題</th></tr>");
-    var topicCount = topic.length;
+var topicCount = 0;
+var millisecsPerDay = 24*60*60*1000;
 
-    var millisecsPerDay = 24*60*60*1000;
+function login() {
+    topicCount = 6;
+    $("#courseTable").empty();
+    $("#add").empty();
+    $("#add").append(`<div><form method = "post" action = "">
+    <input type="text" id="task" required maxlength="10" size="10"><p></p>
+    <input type = "button" id = "submit2" value = "新增" onclick = "stuff()">
+</form></div>`);
+    $("#courseTable").append("<tr><th>場次</th><th>時間</th><th>主題</th></tr>");
 
     var month = $("#month").val();
     var day = $("#date").val();
     setMonthAndDay(month, day);
 
+    var task = $("#task").val();
     for(var x=0;x<topicCount;x++){
         $("#courseTable").append(
         /*"<tr>"+
@@ -19,4 +26,12 @@ function login() {
         `<tr><td>${x+1}</td><td>${new Date(startDate.getTime()+7*x*millisecsPerDay).toLocaleDateString().slice(5)}</td><td>${topic[x]}</td></tr>`);
         }
         
+};
+
+function stuff() {
+    var task = $("#task").val();
+
+    $("#courseTable").append(`<tr><td>${topicCount+1}</td><td>${new Date(startDate.getTime()+7*topicCount*millisecsPerDay).toLocaleDateString().slice(5)}</td><td>${task}</td></tr>`);
+    topicCount+=1;
+    
 };
