8af79d01772514ebbdbeb17942ee79362f01d974
s1101404 <s1101404@mail.yzu.edu.tw>
2022-04-11 18:38:50 +0800

    5

--- assets/js/work4/main.js
+++ assets/js/work4/main.js
@@ -1,4 +1,3 @@
-let mapArray, ctx, currentImgMain;
 let imgMountain, imgMain, imgEnemy;
 //mapArray - 決定地圖中每個格子的元素
 //ctx - HTML5 Canvas用
@@ -6,11 +5,18 @@ let imgMountain, imgMain, imgEnemy;
 //imgMountain, imgMain, imgEnemy - 障礙物, 主角, 敵人的圖片物件
 const gridLength = 200;
 //網頁載入完成後初始化動作
-$(function () {
+function start() {
+    let mapArray, ctx, currentImgMain;
+    let point = 0;
+    $("#start").empty();
+    $("#talk").empty();
+    $("#start").append('<canvas id="myCanvas" width="800" height="800"></canvas>');
+    $("#talk").append(`得分:${point}`);
     mapArray = [ //0-可走,1-障礙,2-終點,3-敵人
-        [0, 1, 1],
-        [0, 0, 0],
-        [3, 1, 2]
+        [0, 1, 1, 0],
+        [0, 0, 4, 4],
+        [3, 4, 4, 0],
+        [1, 0, 1, 2],
     ];
     ctx = $("#myCanvas")[0].getContext("2d");
 
@@ -38,6 +44,9 @@ $(function () {
                     } else if (mapArray[x][y] == 3) {
                         ctx.drawImage(imgEnemy, 7, 40, 104, 135, y * gridLength, x * gridLength, gridLength, gridLength);
                     }
+                    else if (mapArray[x][y] == 4) {
+                        ctx.drawImage(imgMountain, 0, 97, 32, 32, y * gridLength, x * gridLength, gridLength, gridLength);
+                    }
                 }
             }
         }
@@ -54,28 +63,28 @@ $(function () {
             "x": -1,
             "y": -1
         }
-        //event.preventDefault();
+        event.preventDefault();
         //避免鍵盤預設行為發生，如捲動/放大/換頁...
         //判斷使用者按下什麼並推算目標座標
 
         switch (event.code) {
             case
-                "KeyA":
+                "ArrowLeft":
                 targetImg.x = currentImgMain.x - gridLength;
                 targetImg.y = currentImgMain.y;
                 cutImagePositionX = 175;//臉朝左
                 break;
-            case "KeyW":
+            case "ArrowUp":
                 targetImg.x = currentImgMain.x;
                 targetImg.y = currentImgMain.y - gridLength;
                 cutImagePositionX = 355;//臉朝上
                 break;
-            case "KeyD":
+            case "ArrowRight":
                 targetImg.x = currentImgMain.x + gridLength;
                 targetImg.y = currentImgMain.y;
                 cutImagePositionX = 540;//臉朝右
                 break;
-            case "KeyS":
+            case "ArrowDown":
                 targetImg.x = currentImgMain.x;
                 targetImg.y = currentImgMain.y + gridLength;
                 cutImagePositionX = 0;//臉朝下
@@ -84,7 +93,7 @@ $(function () {
                 return;
         }
         //確認目標位置不會超過地圖
-        if (targetImg.x <= 400 && targetImg.x >= 0 && targetImg.y <= 400 && targetImg.y >= 0) {
+        if (targetImg.x <= 600 && targetImg.x >= 0 && targetImg.y <= 600 && targetImg.y >= 0) {
             targetBlock.x = targetImg.y / gridLength;
             targetBlock.y = targetImg.x / gridLength;
         } else {
@@ -92,8 +101,9 @@ $(function () {
             targetBlock.y = -1;
         }
         //清空主角原本所在的位置
+        $("#talk").empty();
+        $("#talk").append(`得分:${point}`);
         ctx.clearRect(currentImgMain.x, currentImgMain.y, gridLength, gridLength);
-
         if (targetBlock.x != -1 && targetBlock.y != -1) {
             switch (mapArray[targetBlock.x][targetBlock.y]) {
                 case 0: // 一般道路(可移動)
@@ -112,6 +122,16 @@ $(function () {
                 case 3: // 敵人(不可移動)
                     $("#talkBox").text("哈摟");
                     break;
+                case 4:
+                    $("#talkBox").text(`好吃`);
+                    currentImgMain.x = targetImg.x;
+                    currentImgMain.y = targetImg.y;
+                    let curx = targetImg.x/200;
+                    let cury = targetImg.y/200;
+                    mapArray[cury][curx] = 0;
+                    point++;
+                    
+                    break;
             }
         } else {
             $("#talkBox").text("邊界");
@@ -119,4 +139,4 @@ $(function () {
         //重新繪製主角
         ctx.drawImage(imgMain, cutImagePositionX, 0, 80, 130, currentImgMain.x, currentImgMain.y, gridLength, gridLength);
     });
-});
+};
